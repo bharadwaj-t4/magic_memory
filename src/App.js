5ab6fa7e7d0e60aec2d702956910ab0f912d9aac
Bharadwaj T <bharadwajtheruvath14@gmail.com>
2022-11-17 13:47:13 +0530

    added matched property to cards

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css'
 import SingleCard from './Components/SingleCard'
 
 const cardImages =[
-  {"src":"/img/helmet-1.png"},
-  {"src":"/img/potion-1.png"},
-  {"src":"/img/ring-1.png"},
-  {"src":"/img/scroll-1.png"},
-  {"src":"/img/shield-1.png"},
-  {"src":"/img/sword-1.png"},
+  {"src":"/img/helmet-1.png", matched:false},
+  {"src":"/img/potion-1.png", matched:false},
+  {"src":"/img/ring-1.png", matched:false},
+  {"src":"/img/scroll-1.png", matched:false},
+  {"src":"/img/shield-1.png", matched:false},
+  {"src":"/img/sword-1.png", matched:false},
 ]
 
 function App() {
@@ -38,14 +38,23 @@ function App() {
   useEffect(()=>{
     if(choiceOne && choiceTwo){
       if(choiceOne.src === choiceTwo.src){
-        console.log("same cards")
+        setCards(prevCards =>{
+          return prevCards.map(card=>{
+            if(card.src === choiceOne.src){
+              return{...card , matched:true}
+            }else{
+              return card
+            }
+          })
+        })
         resetTurn()
       }else{
-        console.log("diff cards")
         resetTurn()
       }
     }
   },[choiceOne , choiceTwo])
+
+  console.log(cards)
 
   //reset choices and increase turn
 
