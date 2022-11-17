21e938922978110b1a2752d767d9da1ec7e2b485
Bharadwaj T <bharadwajtheruvath14@gmail.com>
2022-11-17 12:11:19 +0530

    components added and structure updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import SingleCard from './Components/SingleCard'
 
 const cardImages =[
   {"src":"/img/helmet-1.png"},
@@ -33,12 +34,7 @@ function App() {
 
       <div className='card-grid'>
         {cards.map(card=>(
-          <div className='card' key={card.id}>
-            <div>
-              <img src={card.src} className="front" alt='card-front'/>
-              <img src="/img/cover.png" className="back" alt='card-back'/>
-            </div>
-          </div>
+          <SingleCard key={card.id} card={card}/>
         ))}
       </div>
     </div>
